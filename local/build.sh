f1323adbf2eda094e8343f242486d7939f158ec1
Patrick Langkau <patrick@langkau.info>
2023-03-02 19:27:51 +0100

    change direction

--- local/build.sh
+++ local/build.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
+echo "Installing npm (if not installed)"
+apt-get update
+apt-get install npm -y
+npm install -g n
+n 18.14.2
 cd ..
 cd ..
 
+echo "------------------------------------------------------------"
+echo "Starting to build docker containers"
+echo "------------------------------------------------------------"
+
+echo "------------------------------------------------------------"
+echo "Building backend"
+echo "------------------------------------------------------------"
 cd Backend
 npm install
 docker build . -t webtechnologiesabklbackend
 cd ..
 
-cd duckling
-docker build . -t webtechnologiesabklduckling
-cd ..
-
-cd RASA_Docker
-docker build . -t webtechnologiesabklrasa
-cd ..
-
+echo "------------------------------------------------------------"
+echo "Building WeatherService"
+echo "------------------------------------------------------------"
 cd WeatherService
+npm install
 docker build . -t webtechnologiesabklweatherservice
 cd ..
 
+echo "------------------------------------------------------------"
+echo "Building Website"
+echo "------------------------------------------------------------"
 cd Website
 rm .env.local
 cp .env.local.template .env.local
 sed -i 's/-_dev-prod_-/dev/g' .env.local
+npm install
 docker build . -t webtechnologiesabklwebsite
+cd ..
+
+echo "------------------------------------------------------------"
+echo "Building duckling"
+echo "------------------------------------------------------------"
+cd duckling
+docker build . -t webtechnologiesabklduckling
+cd ..
+
+echo "------------------------------------------------------------"
+echo "Building RASA"
+echo "------------------------------------------------------------"
+cd RASA_Docker
+docker build . -t webtechnologiesabklrasa
 cd ..
 
 cd docker
 cd local
+echo "------------------------------------------------------------"
+echo "Building docker containers finished"
+echo "------------------------------------------------------------"
